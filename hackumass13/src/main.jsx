9d0838e8897011a34da26d35b409a92e7c8a4292
KeplSiv <babu.sivasaran@gmail.com>
2025-11-07 21:33:52 -0500

    v2

--- hackumass13/src/main.jsx
+++ hackumass13/src/main.jsx
@@ -1,11 +1,11 @@
 import { initThemeMode } from "flowbite-react";
-import { StrictMode } from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ThemeInit } from "../.flowbite-react/init";
-import App from "./App.tsx";
+import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeInit />
     <App />
